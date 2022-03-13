092b58168873de7ca0e90b5595dae04c2f11af33
pambrose <pambrose@mac.com>
2022-03-12 22:56:02 -0800

    Add revealjs files for static serving

--- src/main/kotlin/Slides.kt
+++ src/main/kotlin/Slides.kt
@@ -1,50 +1,61 @@
-import com.github.pambrose.*
-import com.github.pambrose.Presentation.Companion.present
-import com.github.pambrose.SlideConfig.Companion.slideConfig
+import com.kslides.*
+import com.kslides.Presentation.Companion.output
+import com.kslides.SlideConfig.Companion.slideConfig
 import kotlinx.html.*
 
 fun main() {
-    presentation {
-        htmlSlide {
-            h1 { +"HTML Slide 🐦" }
-            h2 { +"HTML Slide 🐦" }
-            h3 { +"HTML Slide 🐦" }
-            p { +"This is a test" }
-        }
+  presentation {
 
-        rawHtmlSlide {
-            """
-        <h1>Raw HTML Slide 🐦</h1>
-        <h2>HTML Slide 🐦</h2>
-        <h3>HTML Slide 🐦</h3>
-        <p>This is a test</p>
+    markdownSlide(slideConfig { transition(Transition.Zoom, Speed.Slow) }) {
       """
-        }
-
-        markdownSlide(
-            config = slideConfig { transition(Transition.Zoom, Speed.Slow) },
-            content = """
-          # Markdown Slide 🍒 
-          
-          Press ESC to see presentation overview.
-      """
-        )
-
-        markdownSlide(
-            config = slideConfig { backgroundColor = "#4370A5" },
-            content = """
-          # Code Highlights    
-          ```kotlin [1|2,5|3-4]
-          fun main() {
-              repeat(10) {
-                  println("Hello")
-                  println("World")
-              }
-          }
-          ```
-          """
-        )
+      # Markdown Slide
+      ## 🍒   
+      Press ESC to see presentation overview.
+      """.trimIndent()
     }
 
-    present()
+    htmlSlide {
+      h1 { +"HTML Slide 🐦" }
+      h2 { +"HTML Slide 🐦" }
+      h3 { +"HTML Slide 🐦" }
+      p { +"This is a test" }
+    }
+
+    rawHtmlSlide {
+      """
+      <h1>Raw HTML Slide 🐦</h1>
+      <h2>HTML Slide 🐦</h2>
+      <h3>HTML Slide 🐦</h3>
+      <p>This is a test</p>
+      """
+    }
+
+    markdownSlide(slideConfig { backgroundColor = "#4370A5" }) {
+      """
+        # Code Highlights    
+        ```kotlin [1|2,5|3-4]
+        fun main() {
+            repeat(10) {
+                println("Hello")
+                println("World")
+            }
+        }
+        ```
+        produced with:
+        ````
+        ```kotlin [1|2,5|3-4]
+        fun main() {
+            repeat(10) {
+                println("Hello")
+                println("World")
+            }
+        }
+        ```
+        ````
+      """.trimIndent()
+    }
+  }
+
+  //present()
+  output(dir = "public/slides", srcPrefix = "/../../revealjs/")
 }
